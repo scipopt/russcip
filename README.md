940ef54774b2b5cee51184fb9225817464ca15be
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2023-01-02 19:04:36 +0100

    Add solve function

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # russcip
-A Rust interface for SCIP.
+A Rust interface for SCIP. The project is currently an early-stage work in progress.
+
+## Dependecies 
+make sure [SCIP](https://www.scipopt.org/index.php#download) 8.0.3 is installed and included in the library path.
+
+## Example
+solve a mixed integer program 
+```
+cargo run -- PRBOLEM_PATH
+```
+
+
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,25 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+use std::{ffi::CString, mem::MaybeUninit};
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-fn main() {
+
+fn solve(problem_path: String) {
     unsafe {
-        let mut model: *mut SCIP = std::mem::uninitialized();
-        let x = SCIPcreate(&mut model);
+        let mut model: *mut SCIP = MaybeUninit::uninit().assume_init();
+        SCIPcreate(&mut model);
         SCIPprintVersion(model, std::ptr::null_mut());
+        SCIPincludeDefaultPlugins(model);
+        let filename = CString::new(problem_path).unwrap();
+        SCIPreadProb(model, filename.as_ptr(), std::ptr::null_mut());
+        SCIPsolve(model);
         SCIPfree(&mut model);
     }
+}
+
+fn main() {
+        let filename = std::env::args().nth(1).expect("Expected filename of lp file as an argument");
+        solve(filename) 
 }
