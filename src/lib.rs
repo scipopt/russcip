0d0ebadffe92e8e124e87ce5131b81a440eea293
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2023-01-03 01:43:47 +0100

    Wrap SCIP pointer behind a `Model` struct

---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+use std::{ffi::CString, mem::MaybeUninit};
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+macro_rules! scip_result {
+    ($res:expr) => {
+        let res = unsafe { $res };
+        if res != SCIP_Retcode_SCIP_OKAY {
+            return Err(res);
+        }
+    };
+}
+
+pub struct Model {
+    scip: *mut SCIP,
+}
+
+impl Model {
+    pub fn new() -> Result<Self, SCIP_RETCODE> {
+        let mut model: *mut SCIP = unsafe { MaybeUninit::uninit().assume_init() };
+        scip_result!(SCIPcreate(&mut model));
+        Ok(Model { scip: model })
+    }
+    pub fn include_default_plugins(&mut self) -> Result<(), SCIP_RETCODE> {
+        scip_result! { SCIPincludeDefaultPlugins(self.scip)};
+        Ok(())
+    }
+    pub fn read_prob(&mut self, filename: &str) -> Result<(), SCIP_RETCODE> {
+        let filename = CString::new(filename).unwrap();
+        scip_result! { SCIPreadProb(self.scip, filename.as_ptr(), std::ptr::null_mut()) };
+        Ok(())
+    }
+    pub fn solve(&mut self) -> Result<(), SCIP_RETCODE> {
+        scip_result! { SCIPsolve(self.scip) };
+        Ok(())
+    }
+}
+
+impl Drop for Model {
+    fn drop(&mut self) {
+        unsafe { SCIPfree(&mut self.scip) };
+    }
+}
+
+pub fn solve(problem_path: String) {
+    let mut model = Model::new().unwrap();
+    model.include_default_plugins().unwrap();
+    model.read_prob(&problem_path).unwrap();
+    model.solve().unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,8 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
-use std::{ffi::CString, mem::MaybeUninit};
-
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-
-fn solve(problem_path: String) {
-    unsafe {
-        let mut model: *mut SCIP = MaybeUninit::uninit().assume_init();
-        SCIPcreate(&mut model);
-        SCIPprintVersion(model, std::ptr::null_mut());
-        SCIPincludeDefaultPlugins(model);
-        let filename = CString::new(problem_path).unwrap();
-        SCIPreadProb(model, filename.as_ptr(), std::ptr::null_mut());
-        SCIPsolve(model);
-        SCIPfree(&mut model);
-    }
-}
+use russcip::solve;
 
 fn main() {
-        let filename = std::env::args().nth(1).expect("Expected filename of lp file as an argument");
-        solve(filename) 
+    let filename = std::env::args()
+        .nth(1)
+        .expect("Expected filename of lp file as an argument");
+    solve(filename)
 }
