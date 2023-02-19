ff075864f41408c4f6f5163035d39274b43ff596
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2023-02-19 13:17:35 +0100

    Free scip structs using Drop trait implemenations

--- src/constraint.rs
+++ src/constraint.rs
@@ -2,6 +2,7 @@ use crate::ffi;
 
 pub struct Constraint {
     pub(crate) raw: *mut ffi::SCIP_CONS,
+    pub(crate) scip_ptr: *mut ffi::SCIP,
 }
 
 impl Constraint {
@@ -9,6 +10,14 @@ impl Constraint {
         unsafe {
             let name = ffi::SCIPconsGetName(self.raw);
             String::from(std::ffi::CStr::from_ptr(name).to_str().unwrap())
+        }
+    }
+}
+
+impl Drop for Constraint {
+    fn drop(&mut self) {
+        unsafe {
+            ffi::SCIPreleaseCons(self.scip_ptr, &mut self.raw);
         }
     }
 }
