97fe0dbb31343611c46a7207f399be395d31bd34
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2023-05-08 13:18:04 +0200

    Branching rule, solving stats (#59)

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# CHANGELOG
+
+## Unreleased
+### Added
+- Solving stats methods for number of nodes, time, and number of lp iterations. 
+- Branching rule plugin. 
+### Fixed
+### Changed
+### Removed
--- src/retcode.rs
+++ src/retcode.rs
@@ -52,3 +52,31 @@ impl From<ffi::SCIP_Retcode> for Retcode {
         }
     }
 }
+
+impl From<Retcode> for ffi::SCIP_Retcode {
+    fn from(value: Retcode) -> Self {
+        match value {
+            Retcode::Okay => ffi::SCIP_Retcode_SCIP_OKAY,
+            Retcode::Error => ffi::SCIP_Retcode_SCIP_ERROR,
+            Retcode::NoMemory => ffi::SCIP_Retcode_SCIP_NOMEMORY,
+            Retcode::ReadError => ffi::SCIP_Retcode_SCIP_READERROR,
+            Retcode::WriteError => ffi::SCIP_Retcode_SCIP_WRITEERROR,
+            Retcode::NoFile => ffi::SCIP_Retcode_SCIP_NOFILE,
+            Retcode::FileCreateError => ffi::SCIP_Retcode_SCIP_FILECREATEERROR,
+            Retcode::LpError => ffi::SCIP_Retcode_SCIP_LPERROR,
+            Retcode::NoProblem => ffi::SCIP_Retcode_SCIP_NOPROBLEM,
+            Retcode::InvalidCall => ffi::SCIP_Retcode_SCIP_INVALIDCALL,
+            Retcode::InvalidData => ffi::SCIP_Retcode_SCIP_INVALIDDATA,
+            Retcode::InvalidResult => ffi::SCIP_Retcode_SCIP_INVALIDRESULT,
+            Retcode::PluginNotFound => ffi::SCIP_Retcode_SCIP_PLUGINNOTFOUND,
+            Retcode::ParameterUnknown => ffi::SCIP_Retcode_SCIP_PARAMETERUNKNOWN,
+            Retcode::ParameterWrongType => ffi::SCIP_Retcode_SCIP_PARAMETERWRONGTYPE,
+            Retcode::ParameterWrongVal => ffi::SCIP_Retcode_SCIP_PARAMETERWRONGVAL,
+            Retcode::KeyAlreadyExisting => ffi::SCIP_Retcode_SCIP_KEYALREADYEXISTING,
+            Retcode::MaxDepthLevel => ffi::SCIP_Retcode_SCIP_MAXDEPTHLEVEL,
+            Retcode::BranchError => ffi::SCIP_Retcode_SCIP_BRANCHERROR,
+            Retcode::NotImplemented => ffi::SCIP_Retcode_SCIP_NOTIMPLEMENTED,
+            Retcode::Unknown(val) => val,
+        }
+    }
+}
